5fafa72f8b7ef03cfb8c42fe1280727170f65acb
Lingesh Jayakumar <146103425+Lingesh-coder@users.noreply.github.com>
2026-08-01 10:55:45 +0530

    Refactor input reading in Analytics function

--- analytics/analytics.go
+++ analytics/analytics.go
@@ -11,9 +11,9 @@ import(
 
 func Analytics() (string,error) {
 	fmt.Println("Enter shortURL to view analytics")
-	scan:=bufio.NewReader(os.Stdin)
-	s,_:=scan.ReadString('\n')
-	s=strings.TrimSpace(s)
+	scan := bufio.NewReader(os.Stdin)
+	s,_ := scan.ReadString('\n')
+	s = strings.TrimSpace(s)
 	if _,ok:=ds.Am[s];!ok{
 		return "The short link does not exist",errors.New("404. Not Found")
 	}
