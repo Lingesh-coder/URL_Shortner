752fba39089492786f272c5c6b1942666761e1ae
Lingesh Jayakumar <146103425+Lingesh-coder@users.noreply.github.com>
2026-08-09 11:16:23 +0530

    Format variable declarations in DS.go

--- dss/DS.go
+++ dss/DS.go
@@ -2,10 +2,10 @@ package dss
 
 import "regexp"
 
-var Ma=make(map[string]string)
-var Am=make(map[string]string)
-var AnalyticsMap=make(map[string]int)
+var Ma = make(map[string]string)
+var Am = make(map[string]string)
+var AnalyticsMap = make(map[string]int)
 var MaxURLLen int = 50
-var Size int=6
+var Size int = 6
 var DomainCheck = regexp.MustCompile(`^https:\/\/[A-Za-z0-9.-]+\.[A-Za-z]{2,64}`)
 var CustomURLRegex = regexp.MustCompile(`^[A-Za-z]{6,15}$`)
