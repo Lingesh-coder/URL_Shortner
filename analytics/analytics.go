e06e44e941833c320cc6ca39aa51a1bab633256c
Lingesh Jayakumar <146103425+Lingesh-coder@users.noreply.github.com>
2026-08-19 13:08:12 +0530

    Refactor variable names for clarity in Analytics function

--- analytics/analytics.go
+++ analytics/analytics.go
@@ -12,11 +12,11 @@ import(
 func Analytics() (string,error) {
 	fmt.Println("Enter shortURL to view analytics")
 	scan := bufio.NewReader(os.Stdin)
-	s,_ := scan.ReadString('\n')
-	s = strings.TrimSpace(s)
-	if _,ok := ds.Am[s] ; !ok{
+	str,_ := scan.ReadString('\n')
+	str = strings.TrimSpace(str)
+	if _,ok := ds.Am[str] ; !ok{
 		return "The short link does not exist",errors.New("404. Not Found")
 	}
 	
-	return (s + " is viewed "+string(ds.AnalyticsMap[s]+48) + " time(s)"),nil
+	return (str + " is viewed "+string(ds.AnalyticsMap[str]+48) + " time(s)"),nil
 }
