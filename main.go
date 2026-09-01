79caf867c94e7394b286f739ef5500bc5724f93d
Lingesh <lingeshkumar1515@gmail.com>
2026-09-01 12:48:43 +0530

    Variable naming

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ func main() {
 outer:
 	for {
 		fmt.Println("1. Enter 1 to get a shortened URL\n2. Enter 2 to retrieve the Original URL\n3. Enter 3 to view analytics\n4. Enter 4 to exit")
-		a, err := er.TextToInteger()
+		choice, err := er.TextToInteger()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		switch a {
+		switch choice {
 		case 1:
 			get.Get_URL()
 		case 2:
